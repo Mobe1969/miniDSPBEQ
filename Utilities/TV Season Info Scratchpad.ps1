fb1d17b14391a57da42d0fcb40858a4b9e4b1f74
Mobe1969 <46695255+Mobe1969@users.noreply.github.com>
2021-04-15 12:09:22 +1000

    Additions

--- Utilities/TV Season Info Scratchpad.ps1
+++ Utilities/TV Season Info Scratchpad.ps1
@@ -1,25 +1,15 @@
 ﻿"https://gitlab.com/Mobe1969/beq-reports/-/raw/master" + [uri]::EscapeDataString($report.FullName.Replace("D:\BEQ\Mobe\beq-reports", "").Replace("\", "/")).Replace("%2F", "/")
 
 D:\BEQ\Mobe\beq-reports\TV Series\Black Sails\Season 1
+$id = "1437"
 
-$url = "https://api.themoviedb.org/3/tv/62264?api_key=ac56a60e0c35557f7b8065bc996d77fc&language=en-US&append_to_response=release_dates"
+$id = "34967" # Falling skies
+
+$id = "65708" # Taboo
+$id = "84661" # The Outsider
+
+$url = "https://api.themoviedb.org/3/tv/" + $id + "?api_key=ac56a60e0c35557f7b8065bc996d77fc&language=en-US&append_to_response=release_dates"
 $result = Invoke-RestMethod -Uri $url
-
-
-$result.seasons[0].id
-$result.seasons[0].poster_path
-
-$result.seasons[1].id
-$result.seasons[1].poster_path
-
-
-$result.seasons[2].id
-$result.seasons[2].poster_path
-
-
-$result.seasons[3].id
-$result.seasons[3].poster_path
-
 
 $x = '<beq_season id="' + $result.seasons[0].id + '">
     <number>1</number>
