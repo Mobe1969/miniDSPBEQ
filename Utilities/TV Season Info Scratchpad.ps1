5cc64c332e9b57d384416aa068686fec0212f784
Mobe1969 <46695255+Mobe1969@users.noreply.github.com>
2021-05-06 09:16:32 +1000

    Additions

--- Utilities/TV Season Info Scratchpad.ps1
+++ Utilities/TV Season Info Scratchpad.ps1
@@ -8,7 +8,7 @@ $id = "72844"
 $id = "77236"
 #$id = "65708" # Taboo
 #$id = "84661" # The Outsider
-$id = "71116"
+$id = "46648"
 $url = "https://api.themoviedb.org/3/tv/" + $id + "?api_key=ac56a60e0c35557f7b8065bc996d77fc&language=en-US&append_to_response=release_dates"
 $result = Invoke-RestMethod -Uri $url
 
@@ -41,4 +41,19 @@ $x = '<beq_season id="' + $result.seasons[2].id + '">
     <poster>' + $result.seasons[2].poster_path + '</poster>
     <episodes count="' + $result.seasons[2].episode_count + '"></episodes>
 </beq_season>'
+$x
+
+
+
+$x = '<beq_season id="' + $result.seasons[1].id + '">
+    <number>1</number>
+    <poster>' + $result.seasons[1].poster_path + '</poster>
+    <episodes count="' + $result.seasons[1].episode_count + '"></episodes>
+</beq_season>'
+$x
+$x = '<beq_season id="' + $result.seasons[2].id + '">
+        <number>2</number>
+        <poster>' + $result.seasons[2].poster_path + '</poster>
+        <episodes count="' + $result.seasons[2].episode_count + '"></episodes>
+    </beq_season>'
 $x
