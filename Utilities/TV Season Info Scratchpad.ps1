b5c353005172956c45c28d652216612a88446e49
Mobe1969 <46695255+Mobe1969@users.noreply.github.com>
2021-09-08 09:34:08 +1000

    Update TV Season Info Scratchpad.ps1

--- Utilities/TV Season Info Scratchpad.ps1
+++ Utilities/TV Season Info Scratchpad.ps1
@@ -8,15 +8,15 @@ $id = "72844"
 $id = "77236"
 #$id = "65708" # Taboo
 #$id = "84661" # The Outsider
-$id = "58928"
+$id = "39758"
 $url = "https://api.themoviedb.org/3/tv/" + $id + "?api_key=ac56a60e0c35557f7b8065bc996d77fc&language=en-US&append_to_response=release_dates"
 $result = Invoke-RestMethod -Uri $url
 
-$x = '<beq_season id="' + $result.seasons[0].id + '">
-    <number>1</number>
-    <poster>' + $result.seasons[0].poster_path + '</poster>
-    <episodes count="' + $result.seasons[0].episode_count + '"></episodes>
-</beq_season>'
+$x = '    <beq_season id="' + $result.seasons[0].id + '">
+        <number>1</number>
+        <poster>' + $result.seasons[0].poster_path + '</poster>
+        <episodes count="' + $result.seasons[0].episode_count + '"></episodes>
+    </beq_season>'
 $x
 $x = '<beq_season id="' + $result.seasons[1].id + '">
     <number>2</number>
@@ -37,10 +37,10 @@ $x = '<beq_season id="' + $result.seasons[2].id + '">
 </beq_season>'
 $x
 $x = '<beq_season id="' + $result.seasons[2].id + '">
-    <number>5</number>
-    <poster>' + $result.seasons[2].poster_path + '</poster>
-    <episodes count="' + $result.seasons[2].episode_count + '"></episodes>
-</beq_season>'
+        <number>5</number>
+        <poster>' + $result.seasons[2].poster_path + '</poster>
+        <episodes count="' + $result.seasons[2].episode_count + '"></episodes>
+    </beq_season>'
 $x
 
 
