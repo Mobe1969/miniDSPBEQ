d9a3c9491a79e456e80ba60c9daeeb983e50e221
Mobe1969 <46695255+Mobe1969@users.noreply.github.com>
2021-10-06 16:26:45 +1000

    add

---
+++ Utilities/Completion Check.ps1
@@ -0,0 +1,18 @@
+﻿$signals = "D:\Video\Temp\BEQ\Complete"
+$done = "D:\BEQ\Mobe1969_miniDSPBEQ\Movie BEQs"
+
+$files = Get-ChildItem $signals -Filter *.wav
+if ([System.IO.File]::Exists("D:\BEQ\Errors.txt")) {
+    Clear-Content -Path "D:\BEQ\Errors.txt"
+}
+Add-Content -Path "D:\BEQ\Errors.txt" -Value "Not Done:"
+foreach ($file in $files) {
+    if ($file.Name.Substring(0, 4).Equals("Flat", 3)) {
+        continue
+    }
+    $signalName = [io.path]::GetFileNameWithoutExtension($file.Name)
+    $filter = Get-ChildItem $done -Filter "$signalName.xml"
+    if ($null -eq $filter) {
+        Add-Content -Path "D:\BEQ\Errors.txt" -Value "$($file.Name)"
+    }
+}
---
+++ Utilities/Rename.ps1
@@ -0,0 +1,18 @@
+﻿$signals = "D:\Video\Temp\BEQ\Complete"
+$done = "D:\BEQ\Mobe1969_miniDSPBEQ\Movie BEQs"
+
+$files = Get-ChildItem $signals
+if ([System.IO.File]::Exists("D:\BEQ\Errors.txt")) {
+    Clear-Content -Path "D:\BEQ\Errors.txt"
+}
+Add-Content -Path "D:\BEQ\Errors.txt" -Value "Not Done:"
+foreach ($file in $files) {
+    if ($file.Name -like '*TrueHD Atmos*') {
+        Add-Content -Path "D:\BEQ\Errors.txt" -Value "$($file.Name) to $($file.Name.Replace("TrueHD Atmos", "Atmos"))"
+        #Rename-Item -Path $file.FullName -NewName $file.FullName.Replace("TrueHD Atmos", "Atmos")
+    }
+    if ($file.Name -like '*DTS-X MA*') {
+        Add-Content -Path "D:\BEQ\Errors.txt" -Value "$($file.Name) to $($file.Name.Replace("DTS-X MA", "DTS-X"))"
+        #Rename-Item -Path $file.FullName -NewName $file.Name.Replace("DTS-X MA", "DTS-X")
+    }
+}
