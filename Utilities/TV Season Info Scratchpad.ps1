f19cdd018c2fb307d73001b5eb42f33ec5ef84e9
Mobe1969 <46695255+Mobe1969@users.noreply.github.com>
2021-12-08 05:08:20 +1000

    add

--- Utilities/TV Season Info Scratchpad.ps1
+++ Utilities/TV Season Info Scratchpad.ps1
@@ -1,5 +1,7 @@
-﻿
-$id = "89642"
+﻿if ([System.IO.File]::Exists("D:\BEQ\Errors.txt")) {
+    Clear-Content -Path "D:\BEQ\Errors.txt"
+}
+$id = "83110"
 $url = "https://api.themoviedb.org/3/tv/" + $id + "?api_key=ac56a60e0c35557f7b8065bc996d77fc&language=en-US&append_to_response=release_dates"
 $result = Invoke-RestMethod -Uri $url
 
@@ -9,18 +11,27 @@ $x = '    <beq_season id="' + $result.seasons[0].id + '">
         <episodes count="' + $result.seasons[0].episode_count + '">1,2,3,4,5,6,7,8,9,10,11,12,13</episodes>
     </beq_season>'
 $x
+Add-Content -Path "D:\BEQ\Errors.txt" -Value $x
 $x = '    <beq_season id="' + $result.seasons[1].id + '">
         <number>2</number>
         <poster>' + $result.seasons[1].poster_path + '</poster>
         <episodes count="' + $result.seasons[1].episode_count + '">1,2,3,4,5,6,7,8,9,10,11,12,13</episodes>
     </beq_season>'
 $x
+Add-Content -Path "D:\BEQ\Errors.txt" -Value $x
 $x = '    <beq_season id="' + $result.seasons[2].id + '">
         <number>3</number>
         <poster>' + $result.seasons[2].poster_path + '</poster>
         <episodes count="' + $result.seasons[2].episode_count + '">1,2,3,4,5,6,7,8,9,10,11,12,13</episodes>
     </beq_season>'
 $x
+Add-Content -Path "D:\BEQ\Errors.txt" -Value $x
 
-
+$x = '    <beq_season id="' + $result.seasons[7].id + '">
+        <number>8</number>
+        <poster>' + $result.seasons[7].poster_path + '</poster>
+        <episodes count="' + $result.seasons[7].episode_count + '">1,2,3,4,5,6,7,8,9,10,11,12,13</episodes>
+    </beq_season>'
+$x
+Add-Content -Path "D:\BEQ\Errors.txt" -Value $x
 
