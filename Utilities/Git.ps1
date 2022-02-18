be3ab9c613bc36c5531947bbfc47d410f6dc687f
Mobe1969 <oberhardt@gmail.com>
2022-02-19 06:36:54 +1000

    A

---
+++ Utilities/Git.ps1
@@ -0,0 +1,13 @@
+﻿cd D:\BEQ\Mobe\beq-reports
+
+git pull
+git add .
+git commit --message "A"
+git push -q
+
+cd D:\BEQ\Mobe1969_miniDSPBEQ
+
+git pull
+git add .
+git commit --message "A"
+git push -q
