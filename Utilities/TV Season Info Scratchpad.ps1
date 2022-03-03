dfac046a212dbc3410fe1b0f19d750c481566bbb
Mobe1969 <oberhardt@gmail.com>
2022-03-03 21:01:14 +1000

    A

--- Utilities/TV Season Info Scratchpad.ps1
+++ Utilities/TV Season Info Scratchpad.ps1
@@ -3,7 +3,7 @@
 }
 
 
-$id = "94830"
+$id = "100074"
 $url = "https://api.themoviedb.org/3/tv/" + $id + "?api_key=ac56a60e0c35557f7b8065bc996d77fc&language=en-US&append_to_response=release_dates"
 $result = Invoke-RestMethod -Uri $url
 
@@ -23,6 +23,8 @@ $x = '    <beq_season id="' + $result.seasons[1].id + '">
         <episodes count="' + $result.seasons[1].episode_count + '">1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20</episodes>
     </beq_season>'
 $x
+
+
 Add-Content -Path "D:\BEQ\Errors.txt" -Value $x
 $x = '    <beq_season id="' + $result.seasons[2].id + '">
         <number>3</number>
