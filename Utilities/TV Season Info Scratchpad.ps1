89dd449554d409e11f401f611b1bf122014d6289
Mobe1969 <oberhardt@gmail.com>
2022-04-12 07:49:17 +1000

    A

--- Utilities/TV Season Info Scratchpad.ps1
+++ Utilities/TV Season Info Scratchpad.ps1
@@ -2,12 +2,10 @@
     Clear-Content -Path "D:\BEQ\Errors.txt"
 }
 
-
-$id = "1972"
+$id = "61173"
 $url = "https://api.themoviedb.org/3/tv/" + $id + "?api_key=ac56a60e0c35557f7b8065bc996d77fc&language=en-US&append_to_response=release_dates"
-$x = '    <beq_season id="' + $result.seasons[0].id + '">
 $result = Invoke-RestMethod -Uri $url
-
+$x = '    <beq_season id="' + $result.seasons[0].id + '">
         <number>1</number>
         <poster>' + $result.seasons[0].poster_path + '</poster>
         <episodes count="' + $result.seasons[0].episode_count + '">1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20</episodes>
@@ -40,9 +38,9 @@ $x = '    <beq_season id="' + $result.seasons[3].id + '">
 $x
 Add-Content -Path "D:\BEQ\Errors.txt" -Value $x
 $x = '    <beq_season id="' + $result.seasons[4].id + '">
-        <number>5</number>
+        <number>4</number>
         <poster>' + $result.seasons[4].poster_path + '</poster>
-        <episodes count="' + $result.seasons[4].episode_count + '">1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20</episodes>
+        <episodes count="' + $result.seasons[3].episode_count + '">1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20</episodes>
     </beq_season>'
 $x
 Add-Content -Path "D:\BEQ\Errors.txt" -Value $x
