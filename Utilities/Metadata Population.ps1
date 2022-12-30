83dc998387b05e5cb560557c863ffc6b0764c57f
Mobe1969 <oberhardt@gmail.com>
2022-12-30 19:55:12 +1000

    A

--- Utilities/Metadata Population.ps1
+++ Utilities/Metadata Population.ps1
@@ -502,6 +502,9 @@ foreach ($file in $files) {
         $audio = $fileName.SubString($fileName.IndexOf("AC3"))
         $audio = $audio.Replace("AC3", "DD")
     }
+    elseif ($fileName.IndexOf("DD EX") -ge 0) {
+        $audio = $fileName.SubString($fileName.IndexOf("DD EX"))
+    }
     elseif ($fileName.IndexOf("DD 5") -ge 0) {
         $audio = $fileName.SubString($fileName.IndexOf("DD 5"))
     }
