4a4ab36022ad7e14eccd0a0076e35fd72bc67ccd
Mobe1969 <oberhardt@gmail.com>
2023-03-15 05:15:44 +1000

    A

--- Utilities/TV Season Info Scratchpad.ps1
+++ Utilities/TV Season Info Scratchpad.ps1
@@ -2,7 +2,7 @@
     Clear-Content -Path "D:\BEQ\Errors.txt"
 }
 
-$id = "120998"
+$id = "34967"
 $url = "https://api.themoviedb.org/3/tv/" + $id + "?api_key=ac56a60e0c35557f7b8065bc996d77fc&language=en-US&append_to_response=release_dates"
 $result = Invoke-RestMethod -Uri $url
 $x = '    <beq_season id="' + $result.seasons[0].id + '">
@@ -18,7 +18,7 @@ $x = '    <beq_season id="' + $result.seasons[1].id + '">
         <number>2</number>
         <poster>' + $result.seasons[1].poster_path + '</poster>
         <episodes count="' + $result.seasons[1].episode_count + '">1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20</episodes>
-    </b85486eq_season>'
+    </beq_season>'
 $x
 Add-Content -Path "D:\BEQ\Errors.txt" -Value $x
 
