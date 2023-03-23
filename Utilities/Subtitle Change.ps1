26e4bfbacbc8500864e7e9b5bf2876b88e0dedb8
Mobe1969 <oberhardt@gmail.com>
2023-03-24 07:07:24 +1000

    A

---
+++ Utilities/Subtitle Change.ps1
@@ -0,0 +1,7 @@
+﻿$Search = 'Arial,40,&H00CCCCCC'
+$Replace = 'Arial,40,&H00AAAAAA'
+
+ForEach ($File in (Get-ChildItem -Path 'D:\Video\Library\*.ass' -Recurse -File)) {
+    (Get-Content $File) -Replace $Search,$Replace |
+        Set-Content $File
+}
