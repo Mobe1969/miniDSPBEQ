c989b8f1a72a99828ba9a59e345041577f865390
Mobe1969 <oberhardt@gmail.com>
2023-03-24 11:06:53 +1000

    A

--- Utilities/Subtitle Change.ps1
+++ Utilities/Subtitle Change.ps1
@@ -1,7 +1,18 @@
-﻿$Search = 'Arial,40,&H00CCCCCC'
-$Replace = 'Arial,40,&H00AAAAAA'
+﻿#$Search = 'Arial,40,&H00CCCCCC'
+#$Replace = 'Arial,40,&H00AAAAAA'
 
 ForEach ($File in (Get-ChildItem -Path 'D:\Video\Library\*.ass' -Recurse -File)) {
     (Get-Content $File) -Replace $Search,$Replace |
         Set-Content $File
 }
+
+[RegEx]$Search = '^PlayResY.*\d'
+$Replace = 'PlayResY: 1080'
+ForEach ($File in (Get-ChildItem -Path 'D:\Video\Library\*.ass' -Recurse -File)) {
+    If (Get-Content $File.FullName | Select-String -Pattern "PlayResX: 1920") {
+        write-output "1920x1080"
+        (Get-Content $File) -Replace $Search,$Replace |
+            Set-Content $File
+    }
+}
+
